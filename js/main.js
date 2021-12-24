dc66fcdeb2f1ebe20278fd7c068cb2faffa46f92
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-12-24 14:07:22 -0400

    Adjust desktop styles and functionality

--- js/main.js
+++ js/main.js
@@ -1,13 +1,21 @@
 const questions = document.querySelectorAll(".question");
+const box = document.querySelector(".box");
 
 questions.forEach((question) => {
+  // if (question.classList.contains("active")) {
+  //   box.classList.add("box-active");
+  // } else {
+  //   box.classList.remove("box-active");
+  // }
   question.addEventListener("click", () => {
     question.classList.toggle("active");
     const answer = question.nextElementSibling;
     if (answer.style.display === "block") {
       answer.style.display = "none";
+      box.classList.remove("box-active");
     } else {
       answer.style.display = "block";
+      box.classList.add("box-active");
     }
   });
 });
