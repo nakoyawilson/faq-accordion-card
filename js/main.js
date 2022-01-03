13e9a14bed43eb73472f7f23b56efb321a238c4c
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-01-02 22:17:45 -0400

    Fix functionality for accordian

--- js/main.js
+++ js/main.js
@@ -2,15 +2,20 @@ const questions = document.querySelectorAll(".question");
 const box = document.querySelector(".box");
 
 questions.forEach((question) => {
-  // if (question.classList.contains("active")) {
-  //   box.classList.add("box-active");
-  // } else {
-  //   box.classList.remove("box-active");
-  // }
   question.addEventListener("click", () => {
-    question.classList.toggle("active");
+    const buttonClasses = question.classList.value;
+    questions.forEach((question) => {
+      const answer = question.nextElementSibling;
+      question.classList.remove("active");
+      answer.style.display = "none";
+    });
+    if (buttonClasses === "question active") {
+      question.classList.remove("active");
+    } else {
+      question.classList.add("active");
+    }
     const answer = question.nextElementSibling;
-    if (answer.style.display === "block") {
+    if (!question.classList.contains("active")) {
       answer.style.display = "none";
       box.classList.remove("box-active");
     } else {
